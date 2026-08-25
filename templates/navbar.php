6cd6cd5552e5954393b5d787d74c5a8ffc20012e
leonardo <linus.ard24@gmail.com>
2026-08-25 16:48:50 -0300

    ideia do projeto

---
+++ templates/navbar.php
@@ -0,0 +1,8 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <img src="./assets/images/MinhaVezLogo.png" alt="Logo" width="70" height="30" class="d-inline-block align-top">
+    <a class="navbar-brand" href="index.php">CTP - Gestão</a>
+    <div class="collapse navbar-collapse">
+        <ul class="navbar-nav mr-auto">
+        </ul>
+    </div>
+</nav>
